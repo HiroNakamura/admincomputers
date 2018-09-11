71cd231787368f8c6fa56cb64b1875206fd3f205
ARIEL <carraro.ariel@gmail.com>
2018-09-11 08:58:33 -0500

    JS para datatable de Computadora

---
+++ src/main/resources/static/js/datatableComputadora.js
@@ -0,0 +1,24 @@
+$(document).ready( function () {
+	 var table = $('#computadoraTabla').DataTable({
+			"sAjaxSource": "/rest/computadorasRest",
+			"sAjaxDataProp": "",
+			"order": [[ 0, "asc" ]],
+			"aoColumns": [
+			    { "mData": "bien"},
+		      { "mData": "arrendado" },
+		      { "mData": "asignado" },
+		      { "mData": "ip" },
+		      { "mData": "dns" },
+		      { "mData": "red" },
+		      { "mData": "operativo" },
+		      { "mData": "maquina" },
+		      { "mData": "tipo" },
+		      { "mData": "modelo" },
+		      { "mData": "dominio" },
+		      { "mData": "ubicacion" },
+		      { "mData": "administrador" },
+		      { "mData": "actualizada" },
+		      { "mData": "departamento.nombre" }
+			]
+	 })
+});
