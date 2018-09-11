e74366aa41398a626673bfd11163a60be0f0c16a
ARIEL <carraro.ariel@gmail.com>
2018-09-11 08:39:42 -0500

    Interface crud para Computadora
    Interface crud y paginación para Computadora

---
+++ src/main/java/com/codemonkey/service/ComputadoraCrudService.java
@@ -0,0 +1,13 @@
+package com.codemonkey.service;
+
+import java.util.List;
+import com.codemonkey.entity.Computadora;
+
+public interface ComputadoraCrudService{
+    public List<Computadora> computadorasArrendadas();
+    public List<Computadora> computadorasNoArrendadas();
+
+    public List<Computadora> computadorasByBien(String bien);
+    public List<Computadora> computadorasByArrendado(String arrendado);
+    public List<Computadora> computadorasByIp(String ip);
+}
