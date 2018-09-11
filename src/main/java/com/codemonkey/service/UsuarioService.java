b71f04a915e8e8d2441c778b2a8683edfc1fcb75
ARIEL <carraro.ariel@gmail.com>
2018-09-11 08:36:45 -0500

    Interface servicio para Usuario
    Interface servicio para Usuario

---
+++ src/main/java/com/codemonkey/service/UsuarioService.java
@@ -0,0 +1,12 @@
+package com.codemonkey.service;
+
+import java.util.List;
+import com.codemonkey.entity.Usuario;
+
+public interface UsuarioService{
+    public List<Usuario> getAllUsuarios(); 
+    public Usuario getUsuarioById(long idusuario);
+    public Usuario addUsuario(Usuario usuario);
+    public Usuario updateUsuario(Long idusuario, String apellidos, String cargo, String nombre, String password,String usuario,Long idcomputadora,Long iddepartamento);
+    public void deleteUsuario(long idusuario);
+}
