44eec6dca0a7a97c1d2c61b8b4fc52cd1c436ab9
ARIEL <carraro.ariel@gmail.com>
2018-09-11 08:38:05 -0500

    Interface para entidad Departamento
    Listar departamentos

---
+++ src/main/java/com/codemonkey/service/DepartamentoService.java
@@ -0,0 +1,8 @@
+package com.codemonkey.service;
+
+import java.util.List;
+import com.codemonkey.entity.Departamento;
+
+public interface DepartamentoService {
+        public List<Departamento> getAllDepartamentos();  
+}
