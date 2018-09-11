d6f1b2d31fc4aa6181f9c07305d2a8a91ad93a8a
ARIEL <carraro.ariel@gmail.com>
2018-09-11 08:38:52 -0500

    Interface servicio para Computadora
    Interface servicio para Computadora

---
+++ src/main/java/com/codemonkey/service/ComputadoraService.java
@@ -0,0 +1,12 @@
+package com.codemonkey.service;
+
+import java.util.List;
+import com.codemonkey.entity.Computadora;
+
+public interface ComputadoraService{
+    public List<Computadora> getAllComputadoras(); 
+    public Computadora getComputadoraById(long idcomputadora);
+    public Computadora addComputadora(Computadora computadora);
+    public Computadora updateComputadora(long idcomputadora,String bien,String arrendado, String asignado,String ip, String dns, String red,String operativo,String maquina, String tipo, String modelo,String dominio,String administrador,String ubicacion,boolean actualizada, long iddepartamento);
+    public void deleteComputadora(long idcomputadora);
+}
