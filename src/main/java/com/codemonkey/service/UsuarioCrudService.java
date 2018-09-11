01488243aa0a256a6a38f64932051be0b5f54b15
ARIEL <carraro.ariel@gmail.com>
2018-09-11 08:37:29 -0500

    Interface crud para Usuario
    Interface crud para Usuario y paginación

---
+++ src/main/java/com/codemonkey/service/UsuarioCrudService.java
@@ -0,0 +1,11 @@
+package com.codemonkey.service;
+
+import java.util.List;
+import com.codemonkey.entity.Usuario;
+
+public interface UsuarioCrudService{
+    public List<Usuario> buscarUsuarioByNombre(String nombre);
+    public List<Usuario> buscarUsuarioByUsuario(String usuario);
+    public List<Usuario> buscarUsuarioByComputadoraBien(String computadora);
+    public List<Usuario> buscarUsuarioByComputadoraArrendado(String computadora);
+}
