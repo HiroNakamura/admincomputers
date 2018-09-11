212334f6fcc792d0bf65856699620cc95e45a6bb
ARIEL <carraro.ariel@gmail.com>
2018-09-11 08:56:12 -0500

    Archivo JS

---
+++ src/main/resources/static/js/funciones.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log('%c Javascript activo! ', 'background: #222; color: #bada55');
+
+    document.getElementById("btnImpr").addEventListener('click',function(){
+        alert("Imprimir...");
+    });
+
+     
+
+    /*document.getElementById("Idusuario").readOnly = true;*/
+});
