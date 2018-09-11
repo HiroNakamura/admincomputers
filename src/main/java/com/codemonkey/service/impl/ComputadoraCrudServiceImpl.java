f118ee95fa02cf33d6b56d163ef7034369ab53ba
ARIEL <carraro.ariel@gmail.com>
2018-09-11 08:42:57 -0500

    Implementación de ComputadoraCrudService
    Implementación de ComputadoraCrudService

---
+++ src/main/java/com/codemonkey/service/impl/ComputadoraCrudServiceImpl.java
@@ -0,0 +1,50 @@
+package com.codemonkey.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.codemonkey.entity.Computadora;
+import com.codemonkey.repository.ComputadoraCrudRepository;
+import com.codemonkey.service.ComputadoraCrudService;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Service("computadoraCrudService")
+public class ComputadoraCrudServiceImpl implements ComputadoraCrudService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ComputadoraCrudServiceImpl.class);
+
+    @Autowired
+	private ComputadoraCrudRepository computadoraCrudRepository;
+
+    @Override
+    public List<Computadora> computadorasArrendadas(){
+        return computadoraCrudRepository.computadorasArrendadas();
+    }
+
+    @Override
+    public List<Computadora> computadorasNoArrendadas(){
+        return computadoraCrudRepository.computadorasNoArrendadas();
+    }
+
+    @Override
+    public List<Computadora> computadorasByBien(String bien){
+        return computadoraCrudRepository.computadorasByBien(bien);
+    }
+
+    @Override
+    public List<Computadora> computadorasByArrendado(String arrendado){
+        return computadoraCrudRepository.computadorasByArrendado(arrendado);
+    }
+
+    @Override
+    public List<Computadora> computadorasByIp(String ip){
+        return computadoraCrudRepository.computadorasByIp(ip);
+    }
+
+
+
+}
