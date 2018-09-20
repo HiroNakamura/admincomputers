d3ef189880ae07d674a6bd7c231c47fdf0bd1580
ARIEL <carraro.ariel@gmail.com>
2018-09-20 08:23:16 -0500

    Se agrego nuevo metodo para buscar usuario  IP

--- src/main/java/com/codemonkey/service/UsuarioCrudService.java
+++ src/main/java/com/codemonkey/service/UsuarioCrudService.java
@@ -8,4 +8,5 @@ public interface UsuarioCrudService{
     public List<Usuario> buscarUsuarioByUsuario(String usuario);
     public List<Usuario> buscarUsuarioByComputadoraBien(String computadora);
     public List<Usuario> buscarUsuarioByComputadoraArrendado(String computadora);
+    public List<Usuario> buscarUsuarioByComputadoraIp(String ip);
 }
