4ef5dc2dab2bf575e82c7f58cb15c79c90b3a9ac
ARIEL <carraro.ariel@gmail.com>
2018-09-27 08:51:17 -0500

    JS para consumir REST

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("app", ["ngResource"]);
+ 
+app.controller("appController", function ($scope, $http, dataResource) {
+    $http.get('http://localhost:8080/rest/departamentosRest').success(function (data) {
+        $scope.datos = data;
+    });
+    $scope.datosResource = dataResource.get();
+})
+ 
+app.factory("dataResource", function ($resource) {
+    return $resource("http://localhost:8080/rest/departamentosRest",
+        {},
+        { get: { method: "GET", isArray: true }
+    })
+})
