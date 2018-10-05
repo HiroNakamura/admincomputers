33d6d1a75b5f799892ea70001d9402eca52cee45
ARIEL <carraro.ariel@gmail.com>
2018-10-05 16:26:37 -0500

    lista de dispositivos

--- src/main/java/com/codemonkey/service/ComputadoraCrudService.java
+++ src/main/java/com/codemonkey/service/ComputadoraCrudService.java
@@ -6,6 +6,7 @@ import com.codemonkey.entity.Computadora;
 public interface ComputadoraCrudService{
     public List<Computadora> computadorasArrendadas();
     public List<Computadora> computadorasNoArrendadas();
+    public List<Computadora> computadorasDispositivos();
 
     public List<Computadora> computadorasByBien(String bien);
     public List<Computadora> computadorasByArrendado(String arrendado);
