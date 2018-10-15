f78681ffe88e8fefc42a9911ef6b69d28169749e
ARIEL <carraro.ariel@gmail.com>
2018-10-15 13:50:31 -0500

    Se edito controlador api rest

--- src/main/java/com/codemonkey/controller/ControllerRestProyecto.java
+++ src/main/java/com/codemonkey/controller/ControllerRestProyecto.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable ;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,4 +59,19 @@ public class ControllerRestProyecto{
         return usuarioService.getAllUsuarios();
     }
 
+    /*CRUD Usuario*/
+    @GetMapping("/usuariosRest/{id}")
+    public Usuario getUsuario(@PathVariable("id") long id){
+        LOGGER.info("--Buscas el id:"+id);
+        Usuario encontrado = usuarioService.getUsuarioById(id);
+        if(encontrado == null){
+            return null;
+        }
+        return encontrado;
+    }
+
+    /*CRUD Computadora*/
+
+
 }
+
