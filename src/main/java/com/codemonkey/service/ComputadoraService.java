e45b649961e0505365ae467feb5125fb86174d4e
ARIEL <carraro.ariel@gmail.com>
2018-10-17 08:21:08 -0500

    Se agregó campo dispositivo

--- src/main/java/com/codemonkey/service/ComputadoraService.java
+++ src/main/java/com/codemonkey/service/ComputadoraService.java
@@ -7,6 +7,6 @@ public interface ComputadoraService{
     public List<Computadora> getAllComputadoras(); 
     public Computadora getComputadoraById(long idcomputadora);
     public Computadora addComputadora(Computadora computadora);
-    public Computadora updateComputadora(long idcomputadora,String bien,String arrendado, String asignado,String ip, String dns, String red,String operativo,String maquina, String tipo, String modelo,String dominio,String administrador,String ubicacion,boolean actualizada, long iddepartamento);
+    public Computadora updateComputadora(long idcomputadora,String bien,String arrendado, String asignado,String ip, String dns, String red,String operativo,String maquina, String tipo, String modelo,String dominio,String administrador,String ubicacion,String dispositivo,boolean actualizada, long iddepartamento);
     public void deleteComputadora(long idcomputadora);
 }
