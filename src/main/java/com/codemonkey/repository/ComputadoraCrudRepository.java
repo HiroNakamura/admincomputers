923679c27e090495e63bae9f3ad05fe9ba157e80
ARIEL <carraro.ariel@gmail.com>
2018-10-18 13:20:38 -0500

    Se agregó campo estado

--- src/main/java/com/codemonkey/repository/ComputadoraCrudRepository.java
+++ src/main/java/com/codemonkey/repository/ComputadoraCrudRepository.java
@@ -9,20 +9,19 @@ import com.codemonkey.entity.Computadora;
 
 public interface ComputadoraCrudRepository extends CrudRepository<Computadora, Long>{
 
-    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo FROM public.computadora WHERE public.computadora.bien LIKE '%MXL%' AND public.computadora.dispositivo='Computadora' ",nativeQuery=true)
+    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo, estado FROM public.computadora WHERE public.computadora.bien LIKE '%MXL%' AND public.computadora.dispositivo='Computadora' ",nativeQuery=true)
     public List<Computadora> computadorasArrendadas();
-    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo FROM public.computadora WHERE public.computadora.bien NOT LIKE '%MXL%' AND public.computadora.dispositivo='Computadora'",nativeQuery=true)
+    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo, estado FROM public.computadora WHERE public.computadora.bien NOT LIKE '%MXL%' AND public.computadora.dispositivo='Computadora'",nativeQuery=true)
     public List<Computadora> computadorasNoArrendadas();
-    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo FROM public.computadora WHERE public.computadora.dispositivo!='Computadora'",nativeQuery=true)
+    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo, estado FROM public.computadora WHERE public.computadora.dispositivo!='Computadora'",nativeQuery=true)
     public List<Computadora> computadorasDispositivos();
    
 
-    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo FROM public.computadora WHERE public.computadora.bien LIKE %?1% ",nativeQuery=true)
+    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo, estado FROM public.computadora WHERE public.computadora.bien LIKE %?1% ",nativeQuery=true)
     public List<Computadora> computadorasByBien(String bien);
-    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo FROM public.computadora WHERE public.computadora.arrendado LIKE %?1% ",nativeQuery=true)
+    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo, estado FROM public.computadora WHERE public.computadora.arrendado LIKE %?1% ",nativeQuery=true)
     public List<Computadora> computadorasByArrendado(String arrendado);
-    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo FROM public.computadora WHERE public.computadora.ip LIKE %?1% ",nativeQuery=true)
+    @Query(value="SELECT idcomputadora, actualizada, administrador, arrendado, asignado, bien, departamento, dns, dominio, ip, maquina, modelo, operativo, red, tipo, ubicacion, dispositivo, estado FROM public.computadora WHERE public.computadora.ip LIKE %?1% ",nativeQuery=true)
     public List<Computadora> computadorasByIp(String ip);
-
 
 }
