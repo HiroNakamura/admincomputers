2a4d49952a1df47c99a42822b6f767336f9b8ad2
ARIEL <carraro.ariel@gmail.com>
2018-10-22 10:40:17 -0500

    Actualización

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,6 +3,7 @@ var app = angular.module("app", ["ngResource"]);
 app.controller("appController", function ($scope, $http, dataResource) {
     $http.get('http://localhost:8080/rest/departamentosRest').success(function (data) {
         $scope.datos = data;
+        $scope.titulo = "Departamentos";
     });
     $scope.datosResource = dataResource.get();
 })
