78d9554cc9ed8d9cbdb96245d1ea14442ae757fb
ARIEL <carraro.ariel@gmail.com>
2018-10-30 07:38:39 -0600

    Nueva interface

---
+++ src/main/java/com/codemonkey/service/DepartamentoRestService.java
@@ -0,0 +1,8 @@
+package com.codemonkey.service;
+
+import java.util.List;
+import com.codemonkey.entity.Departamento;
+
+public interface DepartamentoRestService{
+    public List<Departamento> getAllDepartamentos();
+}
