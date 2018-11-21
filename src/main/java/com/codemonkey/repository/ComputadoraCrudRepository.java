ce7e8f109cc95742e5c85bd3aa393b10ed1cb4ea
ARIEL <carraro.ariel@gmail.com>
2018-11-21 13:28:38 -0600

    Update

--- src/main/java/com/codemonkey/repository/ComputadoraCrudRepository.java
+++ src/main/java/com/codemonkey/repository/ComputadoraCrudRepository.java
@@ -25,7 +25,7 @@ public interface ComputadoraCrudRepository extends CrudRepository<Computadora, L
     public List<Computadora> computadorasByIp(String ip);
 
 
-    @Query(value="SELECT distinct emp.nombre, emp.apellidos, emp.cargo, dpto.nombre as area, comp.idcomputadora, comp.bien, comp.arrendado, comp.asignado, comp.administrador, comp.departamento, comp.dns, comp.ip, comp.maquina, comp.modelo, comp.operativo, comp.red, comp.tipo, comp.ubicacion, comp.actualizada, comp.dispositivo, comp.estado FROM public.usuario as emp, public.computadora as comp, public.departamento as dpto WHERE emp.iddepartamento = dpto.iddepartamento AND emp.idcomputadora = comp.idcomputadora AND dpto.nombre like %?1%  ",nativeQuery=true)
+    @Query(value="SELECT distinct emp.nombre, emp.apellidos, emp.cargo, dpto.nombre as area, comp.idcomputadora, comp.bien, comp.arrendado, comp.asignado, comp.administrador, comp.departamento,comp.dominio, comp.dns, comp.ip, comp.maquina, comp.modelo, comp.operativo, comp.red, comp.tipo, comp.ubicacion, comp.actualizada, comp.dispositivo, comp.estado FROM public.usuario as emp, public.computadora as comp, public.departamento as dpto WHERE emp.iddepartamento = dpto.iddepartamento AND emp.idcomputadora = comp.idcomputadora AND dpto.nombre like %?1%  ",nativeQuery=true)
     public List<Computadora> computadorasByDepartamento(String departamento);
     
 
