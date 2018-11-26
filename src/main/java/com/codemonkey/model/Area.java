887c7409e0b9f3d404b0dd37d25193ca4a05ccab
ARIEL <carraro.ariel@gmail.com>
2018-11-26 17:16:19 -0600

    Nueva entidad

---
+++ src/main/java/com/codemonkey/model/Area.java
@@ -0,0 +1,34 @@
+package com.codemonkey.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data 
+@ToString @EqualsAndHashCode
+@Table(name="area")
+@Entity(name="Area")
+public class Area implements java.io.Serializable{
+
+    private static final long serialVersionUID = 8099656478674706638L;
+     
+    @Id
+    @Column(name="idarea")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter private long idarea;
+    @Column(name="nombre")
+    @Getter @Setter private String nombre;
+
+    @OneToMany(mappedBy="area")
+    @Getter @Setter private List<Empleado> empleados;
+
+}
